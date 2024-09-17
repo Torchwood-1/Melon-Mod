c2ebdf4ea729dfe61baab223a471624a02b5c6ac
Sam Tees <26529248+SamTees@users.noreply.github.com>
2024-09-17 12:47:31 +0100

    mess with registry stuff

--- src/main/java/tk/jacobempire/melonmod/common/init/ModBlocks.java
+++ src/main/java/tk/jacobempire/melonmod/common/init/ModBlocks.java
@@ -1,15 +1,16 @@
 package tk.jacobempire.melonmod.common.init;
 
-import net.minecraft.block.*;
+import java.util.function.Supplier;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
-import net.minecraft.block.SoundType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,96 +20,70 @@ import tk.jacobempire.melonmod.common.block.MelonGrassBlock;
 import tk.jacobempire.melonmod.common.world.dimension.SimpleTeleporter;
 
 public class ModBlocks {
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MelonMod.MODID);
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MelonMod.MODID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
+            MelonMod.MODID);
 
-
-    public static final RegistryObject<Block> MELON_STONE = BLOCKS.register("melon_stone",
+    public static final RegistryObject<Block> MELON_STONE = registerBlock("melon_stone",
             () -> new Block(Block.Properties.of(Material.STONE, MaterialColor.STONE)
                     .strength(3.0F, 3.0F)
                     .harvestLevel(2)
                     .requiresCorrectToolForDrops()
-                    .sound(SoundType.STONE)));
+                    .sound(SoundType.STONE)),
+            ItemGroup.TAB_BUILDING_BLOCKS);
 
-    public static final RegistryObject<Block> MOBBLESTONE = BLOCKS.register("mobblestone",
+    public static final RegistryObject<Block> MOBBLESTONE = registerBlock("mobblestone",
             () -> new Block(Block.Properties.of(Material.STONE, MaterialColor.STONE)
                     .strength(3.0F, 3.0F)
                     .harvestLevel(2)
                     .requiresCorrectToolForDrops()
-                    .sound(SoundType.STONE)));
+                    .sound(SoundType.STONE)),
+            ItemGroup.TAB_BUILDING_BLOCKS);
 
-    public static final RegistryObject<Block> MELON_GRASS_BLOCK = BLOCKS.register("melon_grass_block",
+    public static final RegistryObject<Block> MELON_GRASS_BLOCK = registerBlock("melon_grass_block",
             () -> new MelonGrassBlock(Block.Properties.of(Material.GRASS, MaterialColor.COLOR_LIGHT_GREEN)
                     .randomTicks()
                     .strength(0.6F)
-                    .sound(SoundType.GRASS)));
+                    .sound(SoundType.GRASS)),
+            ItemGroup.TAB_BUILDING_BLOCKS);
 
-    public static final RegistryObject<Block> MELON_DIRT = BLOCKS.register("melon_dirt",
+    public static final RegistryObject<Block> MELON_DIRT = registerBlock("melon_dirt",
             () -> new Block(AbstractBlock.Properties.of(Material.DIRT, MaterialColor.COLOR_RED)
                     .strength(0.5F)
-                    .sound(SoundType.GRAVEL)));
+                    .sound(SoundType.GRAVEL)),
+            ItemGroup.TAB_BUILDING_BLOCKS);
 
-//    public static final RegistryObject<Block> MELON_ORE = BLOCKS.register("melon_ore",
-//            () -> new Block(Block.Properties.of(Material.STONE, MaterialColor.STONE)
-//                    .strength(6.0F, 6.0F)
-//                    .harvestLevel(2)
-//                    .sound(SoundType.STONE)));
-
-    public static final RegistryObject<Block> MELON_ORE = BLOCKS.register("melon_ore",
+    public static final RegistryObject<Block> MELON_ORE = registerBlock("melon_ore",
             () -> new SimpleTeleporter(Block.Properties.of(Material.STONE, MaterialColor.STONE)
                     .strength(6.0F, 6.0F)
                     .harvestLevel(2)
-                    .sound(SoundType.STONE)));
+                    .sound(SoundType.STONE)),
+            ItemGroup.TAB_BUILDING_BLOCKS);
 
-    public static final RegistryObject<Block> MELON_GRASS = BLOCKS.register("melon_grass",
+    public static final RegistryObject<Block> MELON_GRASS = registerBlock("melon_grass",
             () -> new MelonBushBlock(AbstractBlock.Properties.of(Material.REPLACEABLE_PLANT)
                     .noCollission()
                     .instabreak()
                     .sound(SoundType.GRASS)
-                    .noOcclusion()));
+                    .noOcclusion()),
+            ItemGroup.TAB_DECORATIONS);
 
-    //TODO: make a method to add blocks and their items at the same time
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, ItemGroup tab) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)));
+    }
 
-    public static final RegistryObject<Item> MELON_STONE_ITEM = ITEMS.register("melon_stone",
-            () -> new BlockItem(MELON_STONE.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
+    @SuppressWarnings("unused")
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        return registerBlock(name, block, ItemGroup.TAB_BUILDING_BLOCKS);
+    }
 
-    public static final RegistryObject<Item> MOBBLESTONE_ITEM = ITEMS.register("mobblestone",
-            () -> new BlockItem(MOBBLESTONE.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
-    public static final RegistryObject<Item> MELON_GRASS_BLOCK_ITEM = ITEMS.register("melon_grass_block",
-            () -> new BlockItem(MELON_GRASS_BLOCK.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
-    public static final RegistryObject<Item> MELON_ORE_ITEM = ITEMS.register("melon_ore",
-            () -> new BlockItem(MELON_ORE.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
-    public static final RegistryObject<Item> MELON_GRASS_ITEM = ITEMS.register("melon_grass",
-            () -> new BlockItem(MELON_GRASS.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
-    public static final RegistryObject<Item> MELON_DIRT_ITEM = ITEMS.register("melon_dirt",
-            () -> new BlockItem(MELON_DIRT.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, ItemGroup tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
-        ITEMS.register(eventBus);
-    }
-
-    @SubscribeEvent
-    public static void registerBlocks(RegistryEvent.Register<Block> event) {
-        event.getRegistry().registerAll(
-                MELON_STONE.get(),
-                MELON_GRASS_BLOCK.get(),
-                MELON_ORE.get(),
-                MOBBLESTONE.get(),
-                MELON_GRASS_BLOCK.get(),
-                MELON_GRASS.get()
-        );
-    }
-
-    @SubscribeEvent
-    public static void registerItems(RegistryEvent.Register<Item> event) {
-        event.getRegistry().registerAll(
-                MELON_STONE_ITEM.get(),
-                MELON_GRASS_BLOCK_ITEM.get(),
-                MELON_ORE_ITEM.get(),
-                MOBBLESTONE_ITEM.get(),
-                MELON_GRASS_BLOCK_ITEM.get(),
-                MELON_GRASS_ITEM.get()
-        );
     }
 }
--- src/main/java/tk/jacobempire/melonmod/common/init/ModItems.java
+++ src/main/java/tk/jacobempire/melonmod/common/init/ModItems.java
@@ -10,74 +10,78 @@ import tk.jacobempire.melonmod.MelonMod;
 import tk.jacobempire.melonmod.common.item.MelonElytra;
 import tk.jacobempire.melonmod.common.material.ModArmorMaterial;
 
-
 import static tk.jacobempire.melonmod.common.item.ModItemTier.MELON;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, MelonMod.MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MelonMod.MODID);
 
-    //Armour
+    // Armour
     public static final RegistryObject<ArmorItem> MELON_HELMET;
     public static final RegistryObject<ArmorItem> MELON_CHESTPLATE;
     public static final RegistryObject<ArmorItem> MELON_LEGGINGS;
     public static final RegistryObject<ArmorItem> MELON_BOOTS;
     public static final RegistryObject<HorseArmorItem> MELON_HORSE_ARMOR;
 
-    //Tools
+    // Tools
     public static final RegistryObject<SwordItem> MELON_SWORD;
     public static final RegistryObject<PickaxeItem> MELON_PICKAXE;
     public static final RegistryObject<AxeItem> MELON_AXE;
     public static final RegistryObject<ShovelItem> MELON_SHOVEL;
     public static final RegistryObject<HoeItem> MELON_HOE;
 
-    //Ingots
+    // Ingots
     public static final RegistryObject<Item> MELON_INGOT;
     public static final RegistryObject<Item> MELON_STICK;
 
-    //Elytra
+    // Elytra
     public static final RegistryObject<MelonElytra> MELON_ELYTRA;
-
 
     public ModItems() {
 
     };
 
     static {
-        //Armour
+        // Armour
         MELON_HELMET = ITEMS.register("melon_helmet",
-            () -> new ArmorItem(ModArmorMaterial.MELON, EquipmentSlotType.HEAD, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+                () -> new ArmorItem(ModArmorMaterial.MELON, EquipmentSlotType.HEAD,
+                        (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
         MELON_CHESTPLATE = ITEMS.register("melon_chestplate",
-            () -> new ArmorItem(ModArmorMaterial.MELON, EquipmentSlotType.CHEST, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+                () -> new ArmorItem(ModArmorMaterial.MELON, EquipmentSlotType.CHEST,
+                        (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
         MELON_LEGGINGS = ITEMS.register("melon_leggings",
-                () -> new ArmorItem(ModArmorMaterial.MELON, EquipmentSlotType.LEGS, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+                () -> new ArmorItem(ModArmorMaterial.MELON, EquipmentSlotType.LEGS,
+                        (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
         MELON_BOOTS = ITEMS.register("melon_boots",
-            () -> new ArmorItem(ModArmorMaterial.MELON, EquipmentSlotType.FEET, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+                () -> new ArmorItem(ModArmorMaterial.MELON, EquipmentSlotType.FEET,
+                        (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
         MELON_HORSE_ARMOR = ITEMS.register("melon_horse_armor",
-           () ->    new HorseArmorItem(11, "melon", (new Item.Properties()).stacksTo(1).tab(ItemGroup.TAB_MISC)));
+                () -> new HorseArmorItem(11, "melon", (new Item.Properties()).stacksTo(1).tab(ItemGroup.TAB_MISC)));
 
-        //Tools
+        // Tools
         MELON_SWORD = ITEMS.register("melon_sword",
-            () -> new SwordItem(MELON, 3, -2.4F, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+                () -> new SwordItem(MELON, 3, -2.4F, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
         MELON_PICKAXE = ITEMS.register("melon_pickaxe",
-            () -> new PickaxeItem(MELON, 1, -2.8F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
+                () -> new PickaxeItem(MELON, 1, -2.8F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
         MELON_AXE = ITEMS.register("melon_axe",
-            () -> new AxeItem(MELON, 5.0F, -3.0F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
+                () -> new AxeItem(MELON, 5.0F, -3.0F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
         MELON_SHOVEL = ITEMS.register("melon_shovel",
-            () -> new ShovelItem(MELON, 1.5F, -3.0F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
+                () -> new ShovelItem(MELON, 1.5F, -3.0F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
         MELON_HOE = ITEMS.register("melon_hoe",
-            () -> new HoeItem(MELON, -3, 0.0F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
+                () -> new HoeItem(MELON, -3, 0.0F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
 
-        //Ingots
+        // Ingots
         MELON_INGOT = ITEMS.register("melon_ingot",
-            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS).food(Foods.MELON_SLICE)));
+                () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS).food(Foods.MELON_SLICE)));
         MELON_STICK = ITEMS.register("melon_stick",
-            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS).food(Foods.MELON_SLICE)));
+                () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS).food(Foods.MELON_SLICE)));
 
-        //Elytra
+        // Elytra
         MELON_ELYTRA = ITEMS.register("melon_elytra",
-            () -> new MelonElytra((new Item.Properties()).durability(432).tab(ItemGroup.TAB_TRANSPORTATION).rarity(Rarity.UNCOMMON)));
+                () -> new MelonElytra((new Item.Properties()).durability(432).tab(ItemGroup.TAB_TRANSPORTATION)
+                        .rarity(Rarity.UNCOMMON)));
     }
 
-    public static void register(IEventBus eventBus) {ITEMS.register(eventBus);}
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
